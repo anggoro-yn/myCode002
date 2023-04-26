d14cf9133481b70cdd14f78a720f2e7aad1840b4
anggoro-yn <70189451+anggoro-yn@users.noreply.github.com>
2023-04-26 09:25:21 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#Import Streamlit library
+import streamlit as st
+#Displaying Markdown
+st.markdown("# Hi,\n# ***People*** \t!!!!!!!!!")
+st.markdown('## Welcome to')
+st.markdown("""### Streamlit's World""")
