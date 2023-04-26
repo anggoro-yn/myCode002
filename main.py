57402a55251a1e7f27a675d0f9a7f23a03d6541c
anggoro-yn <70189451+anggoro-yn@users.noreply.github.com>
2023-04-26 09:30:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,4 @@ st.latex("""(a+b)^2 = a^2 + b^2 + 2ab""")
 st.latex(r'''\frac{\partial u}{\partial t}
 = h^2 \left( \frac{\partial^2 u}{\partial x^2}
 + \frac{\partial^2 u}{\partial y^2}
-Chapter 2 Ttext and table elements
-26
 + \frac{\partial^2 u}{\partial z^2} \right)''')
