4a8ffb9d869ccac84d57573023f3db997b3b8d88
anggoro-yn <70189451+anggoro-yn@users.noreply.github.com>
2023-04-26 11:06:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -44,5 +44,5 @@ import numpy as np
 # defining random values in a dataframe using pandas and numpy
 df = pd.DataFrame(
 np.random.randn(20, 5),
-columns=('col_no %d' % i for i in range(10)))
+columns=('col_no %d' % i for i in range(5)))
 st.dataframe(df)
