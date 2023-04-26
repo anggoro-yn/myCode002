e30081e7ed317c836032258d886d420f543de37f
anggoro-yn <70189451+anggoro-yn@users.noreply.github.com>
2023-04-26 09:29:34 +0700

    Update main.py
    revise code

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #Import Streamlit library
 import streamlit as st
-#Displaying Markdown
-st.markdown("# Hi,\n# ***People*** \t!!!!!!!!!")
-st.markdown('## Welcome to')
-st.markdown("""### Streamlit's World""")
+#Displaying Latex
+st.latex(r'''cos2\theta = 1 - 2sin^2\theta''')
+st.latex("""(a+b)^2 = a^2 + b^2 + 2ab""")
+st.latex(r'''\frac{\partial u}{\partial t}
+= h^2 \left( \frac{\partial^2 u}{\partial x^2}
++ \frac{\partial^2 u}{\partial y^2}
+Chapter 2 Ttext and table elements
+26
++ \frac{\partial^2 u}{\partial z^2} \right)''')
