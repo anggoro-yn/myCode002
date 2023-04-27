0f7cb3342eaafff65f33f55ea8620aa9d39a98d6
anggoro-yn <70189451+anggoro-yn@users.noreply.github.com>
2023-04-27 09:42:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -46,3 +46,10 @@ df = pd.DataFrame(
 np.random.randn(20, 5),
 columns=('col_no %d' % i for i in range(5)))
 st.dataframe(df)
+
+# defining random values in a dataframe using pandas and numpy
+df = pd.DataFrame(
+np.random.randn(20, 5),
+columns=('col_no %d' % i for i in range(5)))
+# Highlighting minimum value objects
+st.dataframe(df.style.highlight_min(axis=0))
