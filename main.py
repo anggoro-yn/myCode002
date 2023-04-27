994bc5dd3c6138a61a3b0dac942d569ee17cd3b2
anggoro-yn <70189451+anggoro-yn@users.noreply.github.com>
2023-04-27 09:46:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ df = pd.DataFrame(
 np.random.randn(20, 5),
 columns=('col_no %d' % i for i in range(5)))
 # Highlighting minimum value objects
-st.dataframe(df.style.highlight_min(axis=1))
+st.dataframe(df.style.highlight_max(axis=1).highlight_min(axis=1))
